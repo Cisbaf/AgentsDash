942774a0bd32cb17ef4debfc63102555bdee75af
GugsFranca <guto602@gmail.com>
2026-03-04 13:54:21 -0300

    Inicializando projeto

---
+++ PainelAgentesBack/src/test/java/com/painelagentesback/PainelAgentesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.painelagentesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelAgentesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
