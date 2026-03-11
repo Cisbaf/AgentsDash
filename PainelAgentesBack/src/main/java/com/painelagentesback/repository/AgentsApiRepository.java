0c6ca38d134b1473cf4201e46d19eb9ca482d11a
GugsFranca <guto602@gmail.com>
2026-03-11 13:32:27 -0300

    feat: Remove unused AgentsApiRepository and CallDetailRepository interfaces

--- PainelAgentesBack/src/main/java/com/painelagentesback/repository/AgentsApiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.painelagentesback.repository;
-
-import com.painelagentesback.models.enitity.AgentDailyStats;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AgentsApiRepository extends JpaRepository<AgentDailyStats, Long> {
-}
